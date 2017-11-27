fe9291888467950d2cad107f8fe255cd2179d3a1
Vahe Sahakyan <vahe@spir.me>
2017-11-27 03:23:13 +0100

    Add pretty json output support

--- filter_json.go
+++ filter_json.go
@@ -14,7 +14,16 @@ func (FilterJSON) Input(in io.Reader, out interface{}, args ...string) error {
 }
 
 func (FilterJSON) Output(out io.Writer, in interface{}, args ...string) error {
-	return json.NewEncoder(out).Encode(in)
+	encoder := json.NewEncoder(out)
+
+	for _, a := range args {
+	    switch a {
+	    case "pretty", "p":
+	        encoder.SetIndent("", "  ")
+	    }
+	}
+
+	return encoder.Encode(in)
 }
 
 const filterJSONDescription = `Input/Output filter for JSON (JavaScript Object Notation), args: none`
