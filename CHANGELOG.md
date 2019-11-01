8eff3e368f4c8bd2feb6b1cfacc31815ab257fad
Vahe Sahakyan <vahe@spir.me>
2019-11-01 02:13:53 +0100

    Update sprig to v3.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
-# v2.0.2 - (unreleased)
+# v2.1.0 - (October 31, 2019)
+ * Bump sprig version to v3.0.0
  * Switch to go 1.13
 
 # v2.0.1 - (October 4, 2019)
--- coder_tpl.go
+++ coder_tpl.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/Masterminds/sprig"
+	sprig "github.com/Masterminds/sprig/v3"
 	"github.com/ashb/jqrepl/jq"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/juju/errors"
